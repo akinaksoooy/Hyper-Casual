0249717e9ac09b2d23ca93983ede661f6276f8c8
akinaksoooy <akinaksoyiletisim@gmail.com>
2022-07-21 23:58:24 +0300

    Hyper Casual oyun dosyaları.

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelController : MonoBehaviour
+{
+    public static LevelController Current;
+    public bool gameActive = false;
+
+    public GameObject startMenu, gameMenu, gameOverMenu, finishMenu;
+    public Text scoreText, finishScoreText, currentLevelText, nextLevelText, startingMenuMoneyText, gameOverMenuMoneyText, finishGameMenuMoneyText;
+    public Slider levelProgressBar;
+    public float maxDistance;
+    public GameObject finishLine;
+
+    public AudioSource gameMusicAudioSource;
+    public AudioClip victoryAudioClip, gameOverAudioClip;
+    public DailyReward dailyReward;
+
+    int currentLevel;
+    int score;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Current = this;
+        currentLevel = PlayerPrefs.GetInt("curentLevel");
+        if (SceneManager.GetActiveScene().name != "Level" + currentLevel)
+        {
+            SceneManager.LoadScene("Level" + currentLevel);
+        }
+        else
+        {
+            dailyReward.InitializeDailyReward();
+            currentLevelText.text = (currentLevel + 1).ToString();
+            nextLevelText.text = (currentLevel + 2).ToString();
+            UpdateMoneyText();
+        }
+        gameMusicAudioSource = Camera.main.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameActive)
+        {
+            PlayerController player = PlayerController.Current;
+            float distance = finishLine.transform.position.z - PlayerController.Current.transform.position.z;
+            levelProgressBar.value = 1 - (distance / maxDistance);  //barın dolması için
+        }
+    }
+
+    public void StartLevel()
+    {
+        maxDistance = finishLine.transform.position.z - PlayerController.Current.transform.position.z;
+        PlayerController.Current.ChangeSpeed(PlayerController.Current.runningSpeed);
+        startMenu.SetActive(false);
+        gameMenu.SetActive(true);
+        PlayerController.Current.animator.SetBool("running", true);
+        gameActive = true;
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene("Level " + (currentLevel + 1));
+    }
+
+    public void GameOver()
+    {
+        UpdateMoneyText();
+        gameMusicAudioSource.Stop();
+        gameMusicAudioSource.PlayOneShot(gameOverAudioClip);
+        gameMenu.SetActive(false);
+        gameOverMenu.SetActive(true);
+        gameActive = false;
+    }
+
+    public void FinishGame()
+    {
+        GiveMoneyToPlayer(score);
+        gameMusicAudioSource.Stop();
+        gameMusicAudioSource.PlayOneShot(victoryAudioClip);
+        PlayerPrefs.SetInt("currentLevel", currentLevel + 1);
+        finishScoreText.text = score.ToString();
+        gameMenu.SetActive(false);
+        finishMenu.SetActive(true);
+        gameActive =false;
+    }
+
+    public void ChangeScore(int increment)
+    {
+        score += increment;
+        scoreText.text = score.ToString();
+    }
+
+    public void UpdateMoneyText()
+    {
+        int money = PlayerPrefs.GetInt("money");
+        startingMenuMoneyText.text = money.ToString();
+        gameOverMenuMoneyText.text = money.ToString();
+        finishGameMenuMoneyText.text = money.ToString();
+    }
+
+    public void GiveMoneyToPlayer(int increment)
+    {
+        int money = PlayerPrefs.GetInt("money");
+        money = Mathf.Max (0, money + increment);
+        PlayerPrefs.SetInt("money", money);
+        UpdateMoneyText();
+    }
+}
